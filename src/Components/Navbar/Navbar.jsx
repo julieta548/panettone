f75be5dcab13a4a414e16e92d3cd00ea5fdb1ce3
julieta548 <julietasosa548@gmail.com>
2023-09-07 09:28:33 -0300

    menu oculto

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className="navbar navbar-expand-lg navbar-light bg-light navegacion">
                 <div className="container-fluid contenedor">
                     <a className="navbar-brand marca" href="#">PANETTONE</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse botones" id="navbarNav">
